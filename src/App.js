c8579885ddee95baade5f42913bad066bdeaa560
Brendan Walker <brenny_walker@hotmail.com>
2021-09-03 10:58:03 -0700

    Adds ProductCard component + styles. Cleans up code base.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import { FAKE_DATA } from "./constants";
+import ProductCard from "./ProductCard";
+
 function App() {
   return (
-    <div>
-      <p className="text-gray-200">Test</p>
+    <div className="flex items-center justify-center space-x-4 my-16">
+      {FAKE_DATA.map(({ id, ...rest }) => (
+          <ProductCard
+            key={id}
+            {...rest}
+          />
+      ))}
     </div>
   );
 }
